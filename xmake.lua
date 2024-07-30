52d05bc69c36c7703217931dd656803ff29650f3
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-07-30 23:26:52 +0100

    Add missing code in xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,3 +1,5 @@
+add_rules("mode.debug", "mode.release")
+
 target("binary_tools")
     set_kind("$(kind)")
     set_languages("c++17")
@@ -6,3 +8,8 @@ target("binary_tools")
     add_headerfiles("include/(**.h)")
 
     add_includedirs("include", {public = true})
+
+    if is_plat("windows") and is_kind("shared") then
+        add_rules("utils.symbols.export_all", {export_classes = true})
+    end
+
